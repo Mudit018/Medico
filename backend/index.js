9579895fa5c17d0c7d38c37c071c053f0b0da7d1
Akshaj Kansal <aknsal@ThinkPad.Dlink>
2021-10-30 19:27:44 +0530

    Add payment route

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
+const cors = require("cors");
 const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
-
+app.use(cors());
 require("./config/passport");
+
 
 const db = "mongodb+srv://aditya:T6guNspYropoREX5@mycluster.wkbqc.mongodb.net/HackOut?retryWrites=true&w=majority";
 if (db) {
@@ -42,6 +44,8 @@ const login = require("./routes/auth");
 app.use("/api", login);
 const appointment = require("./routes/appointment");
 app.use("/appointment",appointment);
+const payment = require("./routes/payment");
+app.use("/payment", payment);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
